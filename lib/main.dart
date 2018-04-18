d4c1724798d877f4feb9f83250a5b8c63e146368
shuheimiyaki <shufei0327@gmail.com>
2018-04-18 09:57:37 +0900

    3. In lib/main.dart, add the import for english_words (again) 4. Use the English words package to generate the text instead of using the string “Hello World”.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -6,6 +7,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
         title: 'Welcome to Flutter',
         home: new Scaffold(
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
                 title: new Text('Welcome to Flutter')
             ),
             body: new Center(
-                child: new Text('Hello World')
+                child: new Text(wordPair.asPascalCase)
             )
         )
     );
